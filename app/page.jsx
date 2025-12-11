22a2e89254434b22227fd94d8c31f87848358a91
spfahy <spfahy@gmail.com>
2025-12-11 14:08:08 -0600

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Metals Dashboard</h1>
+      <p>Select a dataset:</p>
+
+      <ul>
+        <li><a href="/goldcurve">Gold Curve</a></li>
+      </ul>
+    </div>
+  );
+}
