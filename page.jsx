e4b01047eace6cbd9c579f7e6f7167b5faafc948
spfahy <spfahy@gmail.com>
2025-12-24 20:35:55 -0600

    Update page.jsx

--- page.jsx
+++ page.jsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>KMWM Metals Dashboard</h1>
-      <p><a href="/goldcurve">Go to Gold Curve</a></p>
+    <main style={{ padding: 24, fontFamily: "system-ui, Arial" }}>
+      <h1 style={{ marginBottom: 12 }}>KMWM Metals Dashboard</h1>
+      <a href="/goldcurve">Open Gold Curve</a>
     </main>
   );
 }
