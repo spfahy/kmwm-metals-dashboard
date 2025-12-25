0c7ee715a677a0252de6be7343ffc4d2dcfd143b
spfahy <spfahy@gmail.com>
2025-12-24 18:32:05 -0600

    Create page.jsx

---
+++ page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>KMWM Metals Dashboard</h1>
+      <p><a href="/goldcurve">Go to Gold Curve</a></p>
+    </main>
+  );
+}
