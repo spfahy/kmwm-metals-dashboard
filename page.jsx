9e87ad441bdd0badefe7ea821d55f1470b8fc05f
spfahy <spfahy@gmail.com>
2025-12-26 10:01:56 -0600

    Stabilize root page

--- page.jsx
+++ page.jsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <main style={{ padding: 24, fontFamily: "system-ui, Arial" }}>
-      <h1 style={{ marginBottom: 12 }}>KMWM Metals Dashboard</h1>
-      <a href="/goldcurve">Open Gold Curve</a>
+      <h1>KMWM Metals Dashboard</h1>
+      <p>
+        <a href="/goldcurve">Open Gold Curve</a>
+      </p>
     </main>
   );
 }
