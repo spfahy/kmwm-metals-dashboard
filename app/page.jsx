f9ac5572cd40d2ab6872fc03ae34c25090e1237c
spfahy <spfahy@gmail.com>
2025-12-27 13:13:59 -0600

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main style={{ padding: 24, fontFamily: "system-ui, Arial" }}>
       <h1>KMWM Metals Dashboard</h1>
       <p>
-  <a href="/goldcurve">Open Gold Curve</a>
+  <a href="/metals">Open Metals Dashboard</a>
 </p>
 <p>
   <a href="/metals">Open Metals Page 2</a>
